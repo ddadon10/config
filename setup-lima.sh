ba753496e7fa924a24c64569f9cf8bdf321c7b78
David Dadon <david@dadon.fr>
2026-09-20 17:01:41 +0000

    Update setup lima

--- setup-lima.sh
+++ setup-lima.sh
@@ -9,6 +9,3 @@ fi
 limactl list azure >/dev/null 2>&1 || limactl create --tty=false --name=azure lima/azure.yaml
 docker context inspect azure >/dev/null 2>&1 || docker context create azure --docker "host=$(limactl list azure --format 'unix://{{.Dir}}/sock/docker.sock')"
 
-limactl start azure
-docker --context azure build --file docker/Azure.Dockerfile --tag ddadon/azureclient:current .
-limactl stop azure

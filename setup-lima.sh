3c806a61778f2a8f31b4f4e92ac27d0560d3df53
David Dadon <david@dadon.fr>
2026-09-20 17:55:45 +0000

    Update setuplima

--- setup-lima.sh
+++ setup-lima.sh
@@ -6,8 +6,9 @@ if [[ ${1:-} == "--install" ]]; then
   curl -fsSL "${lima_url}" | sudo tar --extract --modification-time --no-same-owner --verbose --directory /usr/local
 fi
 
-limactl list azure >/dev/null 2>&1 || limactl create --yes --name=azure lima/azure.yaml
-docker context inspect azure >/dev/null 2>&1 || docker context create azure --docker "host=$(limactl list azure --format 'unix://{{.Dir}}/sock/docker.sock')"
+limactl list azure >/dev/null 2>&1 || limactl create --tty=false --name=azure lima/azure.yaml
+lima_host=$(limactl list azure --format 'unix://{{.Dir}}/sock/docker.sock')
+docker context inspect azure >/dev/null 2>&1 || docker context create azure --description "Azure CLI environment" --docker "host=${lima_host}"
 
 limactl start azure
 docker --context azure build --file docker/Azure.Dockerfile --tag ddadon/azureclient:current .

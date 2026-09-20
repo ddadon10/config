24e9b76630acba142e8ac7a5f8a731851fe90afe
David Dadon <david@dadon.fr>
2026-09-20 17:06:53 +0000

    Udpate setup lima

--- setup-lima.sh
+++ setup-lima.sh
@@ -6,6 +6,6 @@ if [[ ${1:-} == "--install" ]]; then
   curl -fsSL "${lima_url}" | sudo tar --extract --modification-time --no-same-owner --verbose --directory /usr/local
 fi
 
-limactl list azure >/dev/null 2>&1 || limactl create --tty=false --name=azure lima/azure.yaml
+limactl list azure >/dev/null 2>&1 || limactl create --yes --name=azure lima/azure.yaml
 docker context inspect azure >/dev/null 2>&1 || docker context create azure --docker "host=$(limactl list azure --format 'unix://{{.Dir}}/sock/docker.sock')"
 
